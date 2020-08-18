42620a13743852b11c1d1f06da877373566bec3a
Jeremy Gouveia <avak1908@gmail.com>
2020-08-18 18:55:26 -0400

    Fixed CSS Changes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import '~/assets/styles.scss';
+
 import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/styles.css';
-import '~/assets/custom.scss';
-import '~/assets/styles.scss'
+
+
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
